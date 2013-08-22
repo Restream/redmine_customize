b52ad946bd61583c0432a81814105a8a17e41491
Danil Tashkinov <danil.tashkinov@gmail.com>
2013-08-22 13:23:15 +0400

    fixed load_translations path

--- lib/redmine_customize/patches/i18n_patch.rb
+++ lib/redmine_customize/patches/i18n_patch.rb
@@ -8,11 +8,11 @@ module RedmineCustomize::Patches
       alias_method_chain :load_translations, :env
     end
 
-    # load redmine core locale files first
+    # move redmine plugins locales files to the end of list
     # now plugins can override translations
     def load_translations_with_env(*filenames)
-      core_locales_re = /\A#{Rails.root}\/config\/locales\//
-      sorted_filenames = filenames.flatten.partition { |fn| fn =~ core_locales_re }
+      core_locales_re = /\A#{Rails.root}\/plugins\//
+      sorted_filenames = filenames.flatten.partition { |fn| fn !~ core_locales_re }
       load_translations_without_env sorted_filenames
     end
 
