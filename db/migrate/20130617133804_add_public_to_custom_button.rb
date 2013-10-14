3b0331ec01db799da858bf858433e5cfeb955496
Danil Tashkinov <danil.tashkinov@gmail.com>
2013-10-14 18:14:55 +0400

    fixed default value in migration

--- db/migrate/20130617133804_add_public_to_custom_button.rb
+++ db/migrate/20130617133804_add_public_to_custom_button.rb
@@ -1,5 +1,5 @@
 class AddPublicToCustomButton < ActiveRecord::Migration
   def change
-    add_column :custom_buttons, :is_public, :boolean, :default => 0
+    add_column :custom_buttons, :is_public, :boolean, :default => false
   end
 end
