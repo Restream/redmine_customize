7097f03efb9dc6c601778159ce5b5e8c6187a4f4
Danil Tashkinov <danil.tashkinov@gmail.com>
2014-04-21 15:53:53 +0400

    highlight note when anchor changed

--- assets/javascripts/highlight_note.js
+++ assets/javascripts/highlight_note.js
@@ -1,4 +1,21 @@
 $(document).ready(function(){
-  var noteId = /#note-\d+/.exec(window.location.hash);
-  if (noteId) $(noteId[0] + ", " + noteId[0] + " > h4").effect('highlight', null, 3000);
+  var highlightNote = function() {
+    var noteId = /#note-\d+/.exec(window.location.hash);
+    if (noteId) $(noteId[0] + ", " + noteId[0] + " > h4").effect('highlight', null, 3000);
+  };
+
+  highlightNote();
+
+  if ("onhashchange" in window) { // event supported?
+    window.onhashchange = highlightNote;
+  }
+  else { // event not supported:
+    var storedHash = window.location.hash;
+    window.setInterval(function () {
+      if (window.location.hash != storedHash) {
+        storedHash = window.location.hash;
+        highlightNote();
+      }
+    }, 100);
+  }
 });
