d4137351885348581d62e1c44a39b95c106bd0b9
Danil Tashkinov <danil.tashkinov@gmail.com>
2014-08-01 14:19:35 +0400

    disable page scroll when project quickjump select open

--- assets/javascripts/project_quick_jump_box.js
+++ assets/javascripts/project_quick_jump_box.js
@@ -16,5 +16,20 @@ $(document).ready(function(){
     }
   }).change(function(e) {
     if (e.val != "") { window.location = e.val; }
+  }).on("select2-open", function() {
+    var scrollPosition = [
+        self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+        self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+    ];
+    var html = jQuery('html'); // it would make more sense to apply this to body, but IE7 won't have that
+    html.data('scroll-position', scrollPosition);
+    html.data('previous-overflow', html.css('overflow'));
+    html.css('overflow', 'hidden');
+    window.scrollTo(scrollPosition[0], scrollPosition[1]);
+  }).on("select2-close", function() {
+    var html = jQuery('html');
+    var scrollPosition = html.data('scroll-position');
+    html.css('overflow', html.data('previous-overflow'));
+    window.scrollTo(scrollPosition[0], scrollPosition[1])
   });
 });
